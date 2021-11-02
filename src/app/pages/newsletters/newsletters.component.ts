b1de63f808d05809600c14911e74d7a3db342103
camdenbarta <camdenbarta@gmail.com>
2021-11-01 22:40:27 -0500

    news

--- src/app/pages/newsletters/newsletters.component.ts
+++ src/app/pages/newsletters/newsletters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListService } from 'src/app/services/list.service';
 
 @Component({
   selector: 'app-newsletters',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newsletters.component.scss']
 })
 export class NewslettersComponent implements OnInit {
-
-  constructor() { }
+  filterTerm = "News Letter"
+  news: any;
+  image: any;
+  order=['First','Second','Third','Fourth','Fifth','Sixth'];
+  
+  constructor(
+    private listService: ListService
+  ) { }
 
   ngOnInit(): void {
+    this.retrieveNews()
+  }
+
+  retrieveNews(): void {
+    this.listService.getNews()
+      .subscribe(
+        data => {
+          this.news = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  retrieveImg(): void {
+    this.listService.getImage_Video()
+      .subscribe(
+        data => {
+          this.image = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
